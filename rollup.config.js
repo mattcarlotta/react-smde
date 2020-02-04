2120ef6c63e09baeb6ec6ace921ef0047b763ad1
Matt <carlotta.matt@gmail.com>
2020-02-04 09:59:40 -0800

    Update README and adjust rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -10,37 +10,6 @@ import { terser } from "rollup-plugin-terser";
 import { localResolver } from "./utils/resolver";
 import pkg from "./package.json";
 
-const plugins = [
-  postcss({
-    preprocessor: (_, id) =>
-      new Promise(resolve => {
-        const result = sass.renderSync({ file: id });
-        resolve({ code: result.css.toString() });
-      }),
-    plugins: [autoprefixer],
-    minimize: true,
-    sourceMap: false,
-    extract: pkg.style,
-    extensions: [".sass", ".scss", ".css"]
-  }),
-  babel({
-    runtimeHelpers: true,
-    exclude: "node_modules/**"
-  }),
-  resolve(),
-  localResolver(),
-  commonjs(),
-  terser({
-    output: {
-      comments: "false"
-    }
-  }),
-  copy({
-    targets: [{ src: "src/styles/**/*", dest: "dist/styles" }]
-  }),
-  filesize()
-].filter(Boolean);
-
 const resolutions = {
   globals: {
     react: "React",
@@ -50,30 +19,55 @@ const resolutions = {
   exports: "named"
 };
 
-const output = [
-  {
-    file: pkg.fallback,
-    format: "umd",
-    name: "UMD-MDEditor",
-    ...resolutions
-  },
-  {
-    file: pkg.main,
-    format: "cjs",
-    name: "CJS-MDEditor",
-    ...resolutions
-  },
-  {
-    file: pkg.browser,
-    format: "esm",
-    name: "MDEditor",
-    ...resolutions
-  }
-];
-
 export default {
   input: "./src/index.js",
-  output,
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      ...resolutions
+    },
+    {
+      file: pkg.fallback,
+      format: "umd",
+      name: "MDEditor",
+      ...resolutions
+    },
+    {
+      file: pkg.browser,
+      format: "esm",
+      ...resolutions
+    }
+  ],
   external: ["react", "react-dom", "rc-trigger"],
-  plugins
+  plugins: [
+    postcss({
+      preprocessor: (_, id) =>
+        new Promise(resolve => {
+          const result = sass.renderSync({ file: id });
+          resolve({ code: result.css.toString() });
+        }),
+      plugins: [autoprefixer],
+      minimize: true,
+      sourceMap: false,
+      extract: pkg.style,
+      extensions: [".sass", ".scss", ".css"]
+    }),
+    babel({
+      runtimeHelpers: true,
+      exclude: "node_modules/**"
+    }),
+    resolve(),
+    localResolver(),
+    commonjs(),
+    terser({
+      output: {
+        comments: "false"
+      }
+    }),
+    copy({
+      targets: [{ src: "src/styles/**/*", dest: "dist/styles" }]
+    }),
+    filesize()
+  ]
 };
